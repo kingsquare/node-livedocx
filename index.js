180dfb31d4b35c1a45d7165f900c27f765bba28d
Tim de Koning <github@king-management.nl>
2013-04-12 15:37:22 +0200

    Forked... doesn't work (yet?)

--- index.js
+++ index.js
@@ -1,138 +1,28 @@
 (function () {
 	'use strict';
 
-	var soap, _, url, async, soapSanitize, queue = [];
+	var soap, async, queue = [], process;
 
 	soap = require('soap');
-	_ = require('underscore');
-	url = 'https://api.livedocx.com/2.1/mailmerge.asmx?wsdl';
 	async = require('async');
+	process = require('child_process');
 
-	//crappy soap client can not hanlde accept empty strings!
-	soapSanitize = function (values) {
-		var result = { keys: [], values: [] };
-		_.each(values, function (value, key) {
-			if (!_.isString(value)) {
-				throw new Error(key + ': ' + value + ' is not a string');
-			}
-			if (value !== '') {
-				result.keys.push(key);
-				result.values.push(value);
-			}
-		});
-		return result;
-	};
-
-	//crappy soap client can not operate asynchroneous in any way! If encoding is in progress, WAIT for it to finish!
+	// crappy soap client can not operate asynchroneous in a single process... fork it!
+	// And limit the requests to the server somewhat
 	queue = async.queue(function (options, next) {
-		function log(message) {
-			if (options.debug) {
-				console.log(message);
+		var childProcess = process.fork(__dirname + '/lib/childSoapProcess.js');
+		childProcess.send(options);
+		childProcess.on('message', function(result) {
+			if (result.err) {
+				next(result.err);
+			} else {
+				//a buffer can not be passed as message... decode it here!
+				next(null, new Buffer(result.result, 'base64'));
 			}
-		}
-		log('Starting job');
-		log(options);
-
-		soap.createClient(url, function(err, client) {
-			if (err) {
-				return next(err);
-			}
-			log('Client created');
-
-			client.LogIn({ username: options.username, password: options.password }, function(err) {
-				var tasks = [];
-				if (err) {
-					return next(err);
-				}
-
-				//parallel is not support by soap client
-				tasks.push(function (callback) {
-					client.SetLocalTemplate({
-						format: options.templateFormat,
-						template: options.template
-					}, callback);
-				});
-
-				//sanitizing values may fail!
-				try {
-					// SetBlockFieldValues
-					_.each(options.variables, function (value, key) {
-						var blockFieldConfig;
-						if (_.isArray(value)) {
-							blockFieldConfig = {
-								blockName: key,
-								blockFieldValues: []
-							};
-
-							_.each(value, function (subValue) {
-								var sanitizedValues = soapSanitize(subValue);
-								blockFieldConfig.blockFieldValues.push({
-									ArrayOfString: [{
-										string: sanitizedValues.keys
-									}, {
-										string: sanitizedValues.values
-									}]
-								});
-							});
-
-							tasks.push(function (callback) {
-								client.SetBlockFieldValues(blockFieldConfig, function (err) {
-									log(client.lastRequest);
-									callback(err);
-								});
-							});
-							delete options.variables[key];
-						}
-					});
-				} catch (e) {
-					return next(e);
-				}
-
-				// Set the rest of the values
-				tasks.push(function (callback) {
-					var sanitizedValues;
-					try {
-						sanitizedValues = soapSanitize(options.variables);
-					} catch (e) {
-						return callback(e);
-					}
-					client.SetFieldValues({ fieldValues: [{
-						ArrayOfString: [{
-							string: sanitizedValues.keys
-						}, {
-							string: sanitizedValues.values
-						}]
-					}]}, function (err, result) {
-						log(client.lastRequest);
-						callback(err, result);
-					});
-				});
-
-				//Go!
-				async.series(tasks, function (err) {
-					if (err) {
-						return next(err);
-					}
-					log('All prepared!, creating document');
-					client.CreateDocument({}, function (err) {
-						if (err) {
-							return next(err);
-						}
-						log('Document Created');
-
-						client.RetrieveDocument({ format: options.resultFormat }, function (err, result) {
-							if (err) {
-								return next(err);
-							}
-							log('Document Retrieved');
-
-							return next(null, new Buffer(result.RetrieveDocumentResult, 'base64'));
-						});
-					});
-				});
-			});
+			//kill it!
+			childProcess.send(false);
 		});
-	}, 1);
+	}, 10);
 
 	module.exports = function (options, next) {
 		options.variables = options.variables || {};
